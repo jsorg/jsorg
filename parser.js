7db3ce8d301be8f9c8d6c8b2d9a15ed65d431184
Samira Peri <samira.peri@gmail.com>
2009-09-29 15:43:29 +0300

    parser wibbles

--- parser.js
+++ parser.js
@@ -12,30 +12,41 @@ function empty() {
 }
 
 // Match a given character
-function ch(ch) {
+function ch(chr) {
     return function(s) {
-	if(s == "" || s.charAt(0) != ch)
-	    return { error : "Expected '"+ch+"'", str : s }
+	if(s == "" || s.charAt(0) != chr)
+	    return { error : "Expected '"+chr+"'", str : s }
 	else
-	    return { res : ch, str : s.substr(1) }
+	    return { res : chr, str : s.substr(1) }
     }
 }
 
 // Match any of the given parsers
-function or(ls) {
-    function orAt(ls,idx) {
-	if(ls.length <= idx)
+function or(parsers) {
+    function orAt(idx) {
+	if(parsers.length <= idx)
 	    return function(s) { return { error : "Or!", str : s }}
 	else
 	    return function(s) {
-		var res = ls[idx](s);
+		var res = parsers[idx](s);
 		if(res.res)
 		    return res;
 		else
-		    return orAt(ls,idx+1)(s);
+		    return orAt(idx+1)(s);
 	    }
     }
-    return orAt(ls,0);
+    return orAt(0);
+}
+// optional
+function optional(parser, defaultValue) {
+    //    return or([parser, trans(empty(), function(x) { return defaultValue; })]);
+    return function(s) { 
+	var res = parser(res);
+	if(res.res)
+	    return res;
+	else
+	    return { res: defaultValue, str: s };
+    }
 }
 
 // Apply a function to the parse result
